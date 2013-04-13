71acbff795707ea1b8deeaf1d30efae6993b2c25
Yungoo Kim <yungookim@gmail.com>
2013-04-12 20:12:15 -0400

    OO style js

--- clipboard.js
+++ clipboard.js
@@ -1,16 +1,13 @@
-var copyToClipBoard = function(success, error, text){
-  PhoneGap.exec(
-    function(status, args){ // on success
-      console.log(status + " " + args);
-      success(status, args);
-    }, 
-    function(err){ // on error
-      console.log(err);
-      error(err);
-    }, 
-    "CopyToClipboardPlugin", 
-    "copy", 
-    [text]
+var clipboard = function(){};
+
+var clipboard.prototype.copy = function(text, success, error){
+  PhoneGap.exec(success, error, 
+    "CopyToClipboardPlugin", "copy", [text]
   );
 }
 
+
+
+PhoneGap.addConstructor(function() {
+  PhoneGap.addPlugin('clipboard', new clipboard());
+});
